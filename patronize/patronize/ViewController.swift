b68d3f7538eb85d7ed25dff29908bd3e2dfc32cf
Scott Williams <scott.brandon.williams@gmail.com>
2015-03-07 18:21:02 -0500

    update .gitignore

---
+++ patronize/patronize/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  patronize
+//
+//  Created by Scott Williams on 3/7/15.
+//  Copyright (c) 2015 moltude. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
